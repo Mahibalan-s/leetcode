c21d6906a4b59638163a127991deff21941854a9
Mahibalan-s <149420781+Mahibalan-s@users.noreply.github.com>
2025-08-17 21:17:07 +0530

    [867] [Time Beats: 22.21%] [Memory Beats: 72.30%] - LeetPush

--- Transpose-Matrix.java
+++ Transpose-Matrix.java
@@ -1,12 +1,9 @@
 class Solution {
     public int[][] transpose(int[][] matrix) {
-
-        int a=matrix.length;
-        int b=matrix[0].length;
-        int [][] arr=new int[b][a];
-        for(int i=0;i<b;i++){
-            for(int j=0;j<a;j++){
-                arr[i][j]=matrix[j][i];
+        int[][] arr=new int[(matrix[0].length)][matrix.length];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                arr[j][i]=matrix[i][j];
             }
         }
         return arr;
