1b17fa90922058704f111654f5466a6ba6ff1976
Mahibalan-s <149420781+Mahibalan-s@users.noreply.github.com>
2025-08-25 21:57:32 +0530

    [2500] [Time Beats: 90.65%] [Memory Beats: 15.44%] - LeetPush

---
+++ Delete-Greatest-Value-in-Each-Row.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int deleteGreatestValue(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int sum = 0;
+
+        for (int[] row : grid) {
+            Arrays.sort(row);
+        }
+
+        for (int j = 0; j < n; j++) {
+            int maxVal = 0;
+
+            for (int i = 0; i < m; i++) {
+                maxVal = Math.max(maxVal, grid[i][j]);
+            }
+
+            sum += maxVal;
+        }
+
+        return sum;
+    }
+}
