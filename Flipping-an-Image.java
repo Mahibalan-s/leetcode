844dfa9be664268cda5a40e3d510323311bfc8ea
Mahibalan-s <149420781+Mahibalan-s@users.noreply.github.com>
2026-01-15 18:07:14 +0530

    [832] [Time Beats: 100.00%] [Memory Beats: 10.18%] - LeetPush

--- Flipping-an-Image.java
+++ Flipping-an-Image.java
@@ -7,20 +7,15 @@
 7            int right = n - 1;
 8            
 9            while (left <= right) {
-10                // If the elements are the same, they both need to be inverted
-11                if (row[left] == row[right]) {
-12                    // XOR with 1 flips 0 to 1 and 1 to 0
-13                    int flippedValue = row[left] ^ 1;
-14                    row[left] = flippedValue;
-15                    row[right] = flippedValue;
-16                }
-17                // If they are different, they remain the same after flip + invert
-18                // so we just move the pointers.
-19                
-20                left++;
-21                right--;
-22            }
-23        }
-24        return image;
-25    }
-26}
+10                if (row[left] == row[right]) {
+11                    int flippedValue = row[left] ^ 1;
+12                    row[left] = flippedValue;
+13                    row[right] = flippedValue;
+14                }
+15                left++;
+16                right--;
+17            }
+18        }
+19        return image;
+20    }
+21}
