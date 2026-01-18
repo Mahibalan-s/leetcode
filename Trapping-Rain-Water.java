f2f542ff4df34b5340827fadb74811bbc9a0f0b8
Mahibalan-s <149420781+Mahibalan-s@users.noreply.github.com>
2026-01-18 23:39:50 +0530

    [42] [Time Beats: 100.00%] [Memory Beats: 84.46%] - LeetPush

--- Trapping-Rain-Water.java
+++ Trapping-Rain-Water.java
@@ -1,44 +1,29 @@
 1class Solution {
-2    public int trap(int[] arr) {
-3        if (arr == null || arr.length < 3) return 0;
-4
-5        int sum = 0;
-6        
-7        int peakIndex = 0;
-8        for (int i = 0; i < arr.length; i++) {
-9            if (arr[i] > arr[peakIndex]) {
-10                peakIndex = i;
-11            }
-12        }
-13        int l = 0;
-14        int sub = 0;
-15        int w = 0;
-16        for (int r = 0; r <= peakIndex; r++) {
-17            if (arr[r] >= arr[l]) {
-18                sum += (arr[l] * w) - sub;
-19                l = r;
-20                w = 0;
-21                sub = 0;
-22            } else {
-23                sub += arr[r];
-24                w++;
-25            }
-26        }
-27        int rWall = arr.length - 1;
-28        sub = 0;
-29        w = 0;
-30        for (int i = arr.length - 1; i >= peakIndex; i--) {
-31            if (arr[i] >= arr[rWall]) {
-32                sum += (arr[rWall] * w) - sub;
-33                rWall = i;
-34                w = 0;
-35                sub = 0;
-36            } else {
-37                sub += arr[i];
-38                w++;
-39            }
-40        }
-41
-42        return sum;
-43    }
-44}
+2    public int trap(int[] height) {
+3        if (height == null || height.length == 0) return 0;
+4        
+5        int l = 0, r = height.length - 1;
+6        int leftMax = 0, rightMax = 0;
+7        int totalWater = 0;
+8        
+9        while (l < r) {
+10            if (height[l] < height[r]) {
+11                if (height[l] >= leftMax) {
+12                    leftMax = height[l];
+13                } else {
+14                    totalWater += leftMax - height[l]; 
+15                }
+16                l++;
+17            } else {
+18                if (height[r] >= rightMax) {
+19                    rightMax = height[r];
+20                } else {
+21                    totalWater += rightMax - height[r]; 
+22                }
+23                r--;
+24            }
+25        }
+26        
+27        return totalWater;
+28    }
+29}
