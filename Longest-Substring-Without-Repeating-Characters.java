7ca328a9f5d217eeb8f7698682b77b521e1a4a2a
Mahibalan-s <149420781+Mahibalan-s@users.noreply.github.com>
2026-02-03 22:24:43 +0530

    [3] [Time Beats: 7.28%] [Memory Beats: 5.09%] - LeetPush

--- Longest-Substring-Without-Repeating-Characters.java
+++ Longest-Substring-Without-Repeating-Characters.java
@@ -1,52 +1,35 @@
-
-
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        if (s.equals("")) {
-            return 0;
-        }
-        if (s.trim().isEmpty()) { 
-            return 1;
-        }
-
-        HashMap<Character, Integer> mp = new HashMap<>();
-        int count = 0;
-        int max = 0;
-        int j = 0; // start index of current substring
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
-            if (!mp.containsKey(c)) {
-                mp.put(c, 1);
-                count++;
-            } else {
-                if (mp.get(c) == 0) {
-                    mp.put(c, 1);
-                    count++;
-                } else {
-                    if (count > max) {
-                        max = count;
-                    }
-                    // reset only characters before the duplicate
-                    for (int k = j; k < i; k++) {
-                        if (s.charAt(k) == c) {
-                            j = k + 1;
-                            break;
-                        }
-                        mp.put(s.charAt(k), 0);
-                    }
-                    mp.put(c, 1);
-                    count = i - j + 1;
-                }
-            }
-
-            if (count > max) {
-                max = count;
-            }
-        }
-
-        return max;
-    }
-
-}
+1class Solution {
+2    public int lengthOfLongestSubstring(String s) {
+3        if(s.length()==1){
+4            return 1;
+5        }
+6        else if(s.length()==0){
+7            return 0;
+8        }
+9        int c=1;
+10        int max=Integer.MIN_VALUE;
+11        int i=0;
+12        int j=1;
+13        Map <Character,Integer> hm=new HashMap<>();
+14        hm.put(s.charAt(i),1);
+15        while(j<s.length()){
+16            if(!hm.containsKey(s.charAt(j))){
+17                c++;
+18                hm.put(s.charAt(j),1);
+19                j++;
+20            }
+21            else{
+22                max=Math.max(c,max);
+23                hm.clear();
+24                i++;
+25                j=i+1;
+26                hm.put(s.charAt(i),1);
+27                c=1;
+28            }
+29            
+30        }
+31        max=Math.max(c,max);
+32        return max;
+33    }
+34}
+35
