5bd606dcfb859e35eb8756ca9eca6be5b3b689e2
Mahibalan-s <149420781+Mahibalan-s@users.noreply.github.com>
2026-02-03 22:31:15 +0530

    [3] [Time Beats: 85.64%] [Memory Beats: 96.58%] - LeetPush

--- Longest-Substring-Without-Repeating-Characters.java
+++ Longest-Substring-Without-Repeating-Characters.java
@@ -1,35 +1,22 @@
 1class Solution {
 2    public int lengthOfLongestSubstring(String s) {
-3        if(s.length()==1){
-4            return 1;
-5        }
-6        else if(s.length()==0){
-7            return 0;
-8        }
-9        int c=1;
-10        int max=Integer.MIN_VALUE;
-11        int i=0;
-12        int j=1;
-13        Map <Character,Integer> hm=new HashMap<>();
-14        hm.put(s.charAt(i),1);
-15        while(j<s.length()){
-16            if(!hm.containsKey(s.charAt(j))){
-17                c++;
-18                hm.put(s.charAt(j),1);
-19                j++;
-20            }
-21            else{
-22                max=Math.max(c,max);
-23                hm.clear();
-24                i++;
-25                j=i+1;
-26                hm.put(s.charAt(i),1);
-27                c=1;
-28            }
-29            
-30        }
-31        max=Math.max(c,max);
-32        return max;
-33    }
-34}
-35
+3        ArrayList<Character> arr = new ArrayList<>();
+4        int max = 0;
+5        for (char j : s.toCharArray()) {
+6            if (!arr.contains(j)) {
+7                arr.add(j);
+8
+9            } else {
+10                max = Math.max(max, arr.size());
+11                int idx = arr.indexOf(j);
+12                for (int i = 0; i <= idx; i++) {
+13                    arr.remove(0);
+14                }
+15                arr.add(j);
+16            }
+17        }
+18        max = Math.max(max, arr.size());
+19        return max;
+20
+21    }
+22}
