a0ceec1588b3159cddf84788038a59a166fccd07
Mahibalan-s <149420781+Mahibalan-s@users.noreply.github.com>
2026-02-16 19:41:46 +0530

    [82] [Time Beats: 100.00%] [Memory Beats: 86.75%] - LeetPush

--- Remove-Duplicates-from-Sorted-List-II.java
+++ Remove-Duplicates-from-Sorted-List-II.java
@@ -1,20 +1,20 @@
-class Solution {
-    public ListNode deleteDuplicates(ListNode head) {
-        if (head == null || head.next == null)
-            return head;
-
-        ListNode dummy = new ListNode(0, head);
-
-        ListNode current = dummy;
-        while (current.next != null && current.next.next != null) {
-            if (current.next.val == current.next.next.val) {
-                int duplicate = current.next.val;
-                while (current.next != null && current.next.val == duplicate)
-                    current.next = current.next.next;
-            } else {
-                current = current.next;
-            }
-        }
-        return dummy.next;
-    }
-}
+1class Solution {
+2    public ListNode deleteDuplicates(ListNode head) {
+3        if (head == null || head.next == null)
+4            return head;
+5
+6        ListNode dummy = new ListNode(0, head);
+7
+8        ListNode current = dummy;
+9        while (current.next != null && current.next.next != null) {
+10            if (current.next.val == current.next.next.val) {
+11                int duplicate = current.next.val;
+12                while (current.next != null && current.next.val == duplicate)
+13                    current.next = current.next.next;
+14            } else {
+15                current = current.next;
+16            }
+17        }
+18        return dummy.next;
+19    }
+20}
