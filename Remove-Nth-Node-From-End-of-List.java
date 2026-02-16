d19b9565228dc48b6f345bab65919b908eeec7ed
Mahibalan-s <149420781+Mahibalan-s@users.noreply.github.com>
2026-02-16 19:38:55 +0530

    [19] [Time Beats: 100.00%] [Memory Beats: 89.24%] - LeetPush

--- Remove-Nth-Node-From-End-of-List.java
+++ Remove-Nth-Node-From-End-of-List.java
@@ -1,34 +1,28 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode first = dummy;
-        ListNode second = dummy;
-        
-        // Move first ahead by n+1 steps to maintain a gap of n
-        for (int i = 0; i <= n; i++) {
-            first = first.next;
-        }
-
-        // Move both pointers until first reaches the end
-        while (first != null) {
-            first = first.next;
-            second = second.next;
-        }
-
-        // Remove nth node from end
-        second.next = second.next.next;
-
-        return dummy.next;
-    }
-}
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode removeNthFromEnd(ListNode head, int n) {
+13        ListNode dummy = new ListNode(0);
+14        dummy.next=head;
+15        ListNode t1=dummy;
+16        ListNode t2=dummy;
+17        for(int i=0;i<n;i++){
+18            t1=t1.next;
+19        }
+20        while(t1.next!=null){
+21            t1=t1.next;
+22            t2=t2.next;
+23        }
+24        t2.next=t2.next.next;
+25        return dummy.next;
+26    }
+27}
+28
